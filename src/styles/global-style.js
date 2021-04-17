f2af40317dee32cab31a2ae15f117965e04467e8
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-17 16:30:03 -0300

    chore: add global style

---
+++ src/styles/global-style.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+  
+  * {
+    margin: 0;
+    outline: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+  }
+`;
