62303fadf268e6530eb26f6e0b7cae3e71df0df0
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-17 17:29:14 -0300

    feat: add button component and style

---
+++ src/components/Button/RaisedButton/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import { Wrapper } from './styles'
+
+function RaisedButton({ onPress, text, disabled }) {
+  return (
+    <Wrapper onClick={onPress} disabled={disabled}>
+      {text}
+    </Wrapper>
+  )
+}
+
+RaisedButton.propTypes = {
+  onPress: PropTypes.func,
+  text: PropTypes.string.isRequired,
+  disabled: PropTypes.bool
+}
+
+RaisedButton.defaultProps = {
+  disabled: false
+}
+
+export {RaisedButton}
---
+++ src/components/Button/RaisedButton/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.button`
+  width: 21.75rem;
+  height: 3.563rem;
+  background-color: var(--greeny-blue);
+  border: none;
+  user-select: none;
+  cursor: pointer;
+  transition: opacity 0.2s;
+  font-size: 1.208rem;
+  color: var(--white);
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-transform: uppercase;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:disabled {
+    opacity: 0.4;
+    pointer-events: none;
+  }
+`;
