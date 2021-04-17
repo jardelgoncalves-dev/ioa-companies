608925518e27abb8ff9b65013f5a76c77a2946d2
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-17 16:28:38 -0300

    feat: add storybook config

---
+++ .storybook/preview.js
@@ -0,0 +1,32 @@
+import { ColorsStyle } from "../src/styles/colors";
+import { GlobalStyle } from "../src/styles/global-style";
+
+export const parameters = {
+  backgrounds: {
+    default: "ioasys",
+    values: [
+      {
+        name: "ioasys",
+        value: "#eeecdb",
+      },
+    ],
+  },
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <>
+      <ColorsStyle />
+      <GlobalStyle />
+      <Story {...context} />
+    </>
+  );
+};
+export const decorators = [withThemeProvider];
