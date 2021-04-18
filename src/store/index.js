f1e4e20acbd04dca01f2aef0ef05f097d9237440
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-18 17:15:56 -0300

    feat: add virify authenticate

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,15 @@ import { applyMiddleware, createStore } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
 import thunk from 'redux-thunk'
 
+import {
+  authCallbackRequest,
+  authCallbackError,
+  authCallbackSucess,
+} from './actions/auth/dispatches'
+
 import history from './history'
 import rootReducer from './reducers'
+import { getCredentials, isAuthenticated } from '../services/auth'
 
 const middlewares = [routerMiddleware(history), thunk]
 
@@ -11,5 +18,12 @@ const store = createStore(
   connectRouter(history)(rootReducer),
   applyMiddleware(...middlewares)
 )
+
+store.dispatch(authCallbackRequest())
+if (isAuthenticated()) {
+  store.dispatch(authCallbackSucess(getCredentials()))
+} else {
+  store.dispatch(authCallbackError(''))
+}
 
 export { store }
