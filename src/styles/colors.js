e5389c9e8f7a02dce29667bb0a1733a5fae0fdc3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-17 21:45:58 -0300

    chore: add charcoalgrey, neon-red and grey colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -4,5 +4,9 @@ export const ColorsStyle = createGlobalStyle`
   :root {
     --greeny-blue: #57bbbc;
     --white: #ffffff;
+    --charcoal-grey: #383743;
+    --neon-red: #ff0f44;
+    --grey: #748383;
+
   }
 `
