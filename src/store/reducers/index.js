6bb5f6fc4c8c119fa6383ab877d40fd133672b80
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-18 17:16:59 -0300

    feat: add hisotry to combineReducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+
+import history from '../history'
 
 import auth from './auth'
 
 export default combineReducers({
   auth,
+  router: connectRouter(history),
 })
