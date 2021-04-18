416e40558dfab2bee195d7bdb4fa265f1774d3a3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-17 21:44:39 -0300

    chore: change quote prefer

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,21 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
-  parser: "babel-eslint",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
-    "import/prefer-default-export": "off",
-    "jsx-quotes": ["error", "prefer-single"],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'jsx-quotes': ['error', 'prefer-double'],
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
   },
-};
+}
