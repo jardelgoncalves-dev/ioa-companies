aa9e6fe093b51baa3a9083cf395579b33b4f9e4c
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-18 12:55:47 -0300

    feat: add interation to login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,29 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import { withLogic } from './withLogic';
+
 import { Input } from '../../components/Input'
 import { RaisedButton } from '../../components/Button'
-
+import { Loading } from '../../components/Loading'
 import { Wrapper } from './styles'
+
+import * as AuthAction from "../../store/actions/auth/index";
 
 import LogoImg from '../../assets/logo-home.png'
 
-function Login() {
+function Login({
+  form,
+  passwordVisible,
+  showPassword,
+  onInputChange,
+  onSubmit,
+  authenticate,
+  authState,
+}) {
   return (
       <Wrapper>
+        <Loading isLoading={authState.isLoading} />
         <div className="login-page__content">
           <img className="login-page__content__img" src={LogoImg} alt="Ioasys Logo" />
           <div className="login-page__content__description">
@@ -19,16 +35,63 @@ function Login() {
             </p>
           </div>
           <div className="login-page__content__form">
-            <Input placeholder="Email" iconLeft="email" />
-            <Input placeholder="Senha" iconLeft="padlock" iconRight="eye" />
-            <div className="login-page__content__form__error">
-              Credenciais informadas são inválidas, tente novamente.
-            </div>
-            <RaisedButton text="Entrar" />
+            <Input
+              withError={!!authState.errorMessage}
+              name="email"
+              placeholder="Email"
+              iconLeft="email"
+              onChange={onInputChange}
+            />
+            <Input
+              withError={!!authState.errorMessage}
+              name="password"
+              type={passwordVisible ? 'text' : 'password'}
+              placeholder="Senha"
+              iconLeft="padlock"
+              iconRight={passwordVisible ? 'eye' : 'eye-off'}
+              onChange={onInputChange}
+              iconRightConfig={{
+                clickable: true,
+                onClick: showPassword,
+              }}
+            />
+            {authState.errorMessage && (
+              <div className="login-page__content__form__error">
+                {authState.errorMessage}
+              </div>
+            )}
+            <RaisedButton
+              text="Entrar"
+              onPress={() => onSubmit(form, authenticate)}
+              disabled={!!(!form || !form.email || !form.password)}
+            />
           </div>
         </div>
       </Wrapper>
   )
 }
 
-export { Login }
+Login.propTypes = {
+  authenticate: PropTypes.func,
+  onInputChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  showPassword: PropTypes.func,
+  passwordVisible: PropTypes.bool,
+  form: PropTypes.shape({
+    email: PropTypes.string,
+    password: PropTypes.string,
+  }),
+  authState: PropTypes.shape({
+    errorMessage: PropTypes.string,
+    isLoading: PropTypes.bool,
+  })
+}
+
+const mapStateToProps = (state) => ({
+  authState: state.auth,
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ ...AuthAction, }, dispatch);
+
+export default withLogic(connect(mapStateToProps, mapDispatchToProps)(Login));
