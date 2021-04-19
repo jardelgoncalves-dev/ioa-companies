920c3d3fb152fc99c38a170492beb409b6adcb18
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-19 01:04:11 -0300

    docs: remove properties unanecessary

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -13,20 +13,7 @@ Card.args = {
   item: {
     id: 1,
     enterprise_name: 'Fluoretiq Limited',
-    description:
-      'FluoretiQ is a Bristol based medtech start-up developing diagnostic technology to enable bacteria identification within the average consultation window, so that patients can get the right anti-biotics from the start.  ',
-    email_enterprise: null,
-    facebook: null,
-    twitter: null,
-    linkedin: null,
-    phone: null,
-    own_enterprise: false,
     photo: '/uploads/enterprise/photo/1/240.jpeg',
-    value: 0,
-    shares: 100,
-    share_price: 10000.0,
-    own_shares: 0,
-    city: 'Bristol',
     country: 'UK',
     enterprise_type: {
       id: 3,
