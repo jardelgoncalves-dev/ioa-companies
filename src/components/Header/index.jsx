6419fcf630b71f1f0e114c74d37f84f821e45eb0
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-19 00:46:12 -0300

    feat: add goback version

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,24 +3,51 @@ import { Wrapper } from './styles'
 
 import LogoWhiteImg from '../../assets/logo-white.png'
 import { SearchBar } from '../SearchBar'
+import { Icon } from '../Icon'
 
-function Header({ onSearch, valueSearch, onSearchClose }) {
+function Header({
+  onSearch,
+  valueSearch,
+  onSearchClose,
+  isGoback,
+  onGoBack,
+  textGoBack,
+}) {
   const [expanded, setExpanded] = useState(false)
   return (
     <Wrapper>
-      <div className={`ioa-header__content ${expanded ? '--no-logo': ''}`}>
-        <img src={LogoWhiteImg} alt="Logo" className="ioa-header__content__img" />
-        <SearchBar
-          placeholder="Pesquisar"
-          expanded={expanded}
-          value={valueSearch}
-          onChange={onSearch}
-          onExpand={() => setExpanded(true)}
-          onClose={() => {
-            setExpanded(false);
-            onSearchClose()
-          }}
-        />
+      <div
+        className={`ioa-header__content ${isGoback ? '--go-back': ''} ${expanded ? '--no-logo': ''}`}>
+        {!isGoback && (
+          <img
+            src={LogoWhiteImg}
+            alt="Logo"
+            className="ioa-header__content__img"
+          />
+        )}
+        {!isGoback && (
+          <SearchBar
+            placeholder="Pesquisar"
+            expanded={expanded}
+            value={valueSearch}
+            onChange={onSearch}
+            onExpand={() => setExpanded(true)}
+            onClose={() => {
+              setExpanded(false);
+              onSearchClose && onSearchClose()
+            }}
+          />
+        )}
+        {isGoback && (
+          <>
+            <button type="button" className="go-back-btn" onClick={onGoBack}>
+              <Icon name="arrow-left" />
+            </button>
+            <h3 className="title-header">
+              {textGoBack}
+            </h3>
+          </>
+        )}
       </div>
     </Wrapper>
   )
