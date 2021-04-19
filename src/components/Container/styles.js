7cc7dfa7a19e9b2ff1657df42aec90b6c7227f97
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-18 23:03:36 -0300

    refactor: add flex wrap

--- src/components/Container/styles.js
+++ src/components/Container/styles.js
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
   width: 100%;
-  height: 100%;
-  padding: 0 20px 60px 20px;
+  padding: 20px 20px 60px 20px;
 `
