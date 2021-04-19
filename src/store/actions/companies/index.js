57dfc70a8cf442b3ef3a80fab96de6d7f6669441
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-18 23:05:12 -0300

    feat: add actions to companies

---
+++ src/store/actions/companies/index.js
@@ -0,0 +1,34 @@
+import _ from 'lodash'
+import {
+  companiesCallbackRequest,
+  companiesCallbackError,
+  companiesCallbackSucess,
+} from './dispatches'
+import api from '../../../services/api'
+
+import { removeCredentials } from '../../../services/auth'
+
+const debounceFn = _.debounce(
+  (dispatch, query) =>
+    setTimeout(async () => {
+      try {
+        await dispatch(companiesCallbackRequest())
+        if (!query) {
+          await dispatch(companiesCallbackSucess([]))
+          return
+        }
+        const { data } = await api.get(`/enterprises?name=${query}`)
+        await dispatch(companiesCallbackSucess(data.enterprises))
+      } catch (error) {
+        if (error.response && error.response.status === 409) {
+          removeCredentials()
+          await dispatch(companiesCallbackError(error.response.data.errors[0]))
+        } else {
+          await dispatch(companiesCallbackError('Error interno do servidor'))
+        }
+      }
+    }, 300),
+  400
+)
+
+export const fetch = (query) => (dispatch) => debounceFn(dispatch, query)
